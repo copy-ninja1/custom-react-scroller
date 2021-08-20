01b326428542d6256ba59eaa7f17f5a4220ee0fa
copy-ninja1 <emmajnr1000@gmail.com>
2021-08-21 00:31:44 +0100

    Updates

---
+++ precache-manifest.3db6d2a1335b1fed7078de3d880aef28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8361316b5a40f6605a649c631fa5437e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c7131bf79e5cc1ee9fd",
+    "url": "./static/css/main.3db9280b.chunk.css"
+  },
+  {
+    "revision": "fd3ced0e5436a0a326db",
+    "url": "./static/js/2.7e22558f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7e22558f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7131bf79e5cc1ee9fd",
+    "url": "./static/js/main.c38f56ac.chunk.js"
+  },
+  {
+    "revision": "c86eec11fb1c01ea5c70",
+    "url": "./static/js/runtime-main.35bc169f.js"
+  }
+]);
