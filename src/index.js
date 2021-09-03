766da375a637f0f6fbf4879c516116a244515163
copy-ninja1 <emmajnr1000@gmail.com>
2021-09-03 23:38:00 +0100

    fix: propsleftIcon is not defined

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import React from 'react'
 import Slider from './slider'
-// import styles from './styles.module.css'
 
-// export const ExampleComponent = ({ text }) => {
-//   return <div className={styles.test}>Example Component: {text}</div>
-// }
 export default Slider
